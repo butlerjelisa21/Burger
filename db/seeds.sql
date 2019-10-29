e5efe5b0b7fdb1f8fe98368ce6e8921a65b692b5
Jelisa Butler <jelisa.l.butler@gmail.com>
2019-10-29 17:53:42 -0500

    got schema and seeds running

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ("Emergency Krabby Patty");
+
+INSERT INTO burgers (burger_name) VALUES ("Krabby Patty in a Can");
+
+INSERT INTO burgers (burger_name) VALUES ("All-Bun Patty");
+
+INSERT INTO burgers (burger_name) VALUES ("Krabby Double Deluxe");
+
+INSERT INTO burgers (burger_name) VALUES ("Double Triple Bossy Deluxe");
